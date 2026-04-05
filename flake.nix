91250c2acdc533a1deb545f77e37bcd3ccf534df
rabiibanine <rbanine@gmail.com>
2026-04-05 14:49:31 +0100

    Syntax fix

--- flake.nix
+++ flake.nix
@@ -7,22 +7,19 @@
     home-manager = {
       url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
-      };
+    };
   };
 
   outputs = { self, nixpkgs, home-manager } @ inputs:
     let
-      
       system = "x86_64-linux";
-
     in
-
     {
       nixosConfigurations.pizzahub = nixpkgs.lib.nixosSystem {
         specialArgs = { inherit inputs; };
-	modules = [
-	  ./configuration.nix
-	];
+        modules = [
+          ./configuration.nix
+        ];
       };
     };
-};
+}
