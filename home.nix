30f7fecf31a115e21c85eaba88a184ef02d37f63
rabiibanine <rbanine@gmail.com>
2026-04-06 10:32:38 +0100

    Add ideaVim config

--- home.nix
+++ home.nix
@@ -40,7 +40,7 @@
     enable = true;
     font = {
       name = "JetBrainsMono Nerd Font";
-      size = 12;
+      size = 16;
     };
     settings = {
       scrollback_lines = 10000;
@@ -54,5 +54,28 @@
   home.packages = [
     pkgs.jetbrains.idea-ultimate
   ];
+
+  home.file.".ideavimrc".text = ''
+    "" --- Keybindings (Syncing with your Neovim) ---
+    let mapleader = " "
+
+    " Fast exit from insert mode
+    inoremap jk <Esc>
+    inoremap kj <Esc>
+
+    " Save and Quit (Mapping to IntelliJ Actions)
+    " nnoremap <leader>w <Action>(SaveAll)
+    " nnoremap <leader>q <Action>(CloseContent)
+
+    "" --- Quality of Life ---
+    set number
+    set relativenumber
+    set ideajoin      " Use IntelliJ's smart join logic
+    set ideaput      " Use IntelliJ's clipboard logic
+    set surround     " Enables vim-surround support
+    set hlsearch
+    set ignorecase
+    set smartcase
+  '';
 
 }
