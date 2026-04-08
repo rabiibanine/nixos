a2c2b3f38494fe823b096094be64cd03df2cbab6
rabiibanine <rbanine@gmail.com>
2026-04-08 16:22:55 +0100

    Update firefox with profile and ublock

--- home.nix
+++ home.nix
@@ -25,6 +25,29 @@
 
   programs.firefox = {
     enable = true;
+
+    profiles.pizzakat = {
+
+      id = 0;
+      name = "pizzakat";
+      isDefault = true;
+
+      settings = {
+
+      };
+
+    };
+
+    policies = {
+
+      ExtensionSettings = {
+
+        "uBlock0@raymondhill.net" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+          installation_mode = "force_installed";
+        };
+
+    };
   };
 
   programs.neovim = {
