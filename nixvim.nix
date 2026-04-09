fd146de3eddcc5f03d25bd2125eb5a60f9eff502
rabiibanine <rbanine@gmail.com>
2026-04-09 17:33:16 +0100

    Add lsp with a few languages

--- nixvim.nix
+++ nixvim.nix
@@ -57,6 +57,45 @@
         enable = true; 
       };
 
+      lsp = {
+        enable = true;
+
+        servers = {
+          ts_ls.enable = true;
+          nixd.enable = true;
+
+          # Web
+          html.enable = true;
+          cssls.enable = true;
+
+          # Python
+          pyright.enable = true;
+
+          # C/C++
+          clangd.enable = true;
+
+          # Java
+          jdtls.enable = true;
+        };
+
+        keymaps = {
+          # Diagnostic keymaps (for navigating errors/warnings)
+          diagnostic = {
+            "<leader>j" = "goto_next";
+            "<leader>k" = "goto_prev";
+          };
+
+          # Buffer keymaps (actions you take on the code itself)
+          lspBuf = {
+            "gd" = "definition";         # Go to definition
+            "gr" = "references";         # Find all references
+            "K"  = "hover";              # Show documentation/types (Shift+k)
+            "<leader>rn" = "rename";     # Rename variable across entire project
+            "<leader>ca" = "code_action";# Auto-fix/Code actions
+          };
+        };
+      };
+
     };
 
   };
