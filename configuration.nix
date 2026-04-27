159a07779c32ffc4a1c3d2cfdc1b7d385f505585
rabiibanine <rbanine@gmail.com>
2026-04-27 01:53:08 +0100

    Fix postgres connections

--- configuration.nix
+++ configuration.nix
@@ -52,8 +52,14 @@
     enable = true;
     ensureDatabases = [ "mydatabase" ];
     authentication = pkgs.lib.mkOverride 10 ''
-      #type database  DBuser  auth-method
-      local all       all     trust
+      # TYPE  DATABASE        USER            ADDRESS                 METHOD
+
+      # "local" is for Unix domain socket connections only
+      local   all             all                                     scram-sha-256
+      # IPv4 local connections:
+      host    all             all             127.0.0.1/32            scram-sha-256
+      # IPv6 local connections:
+      host    all             all             ::1/128                 scram-sha-256
     '';
   };
 
