71be21725eb69c1e6fb74b870963216af9e6ab85
rabiibanine <rbanine@gmail.com>
2026-06-01 14:27:09 +0100

    chore: re enable eslint

--- nixvim.nix
+++ nixvim.nix
@@ -166,7 +166,7 @@
           # Web
           html.enable = true;
           cssls.enable = true;
-          eslint.enable = false; # TODO Remember to enable this
+          eslint.enable = true;
 
           # Python
           pyright.enable = true;
@@ -225,18 +225,21 @@
         enable = true;
         settings = {
           formatters_by_ft = {
-            # TODO Re add eslint_d to all of these
             javascript = [
               "prettier"
+              "eslint_d"
             ];
             typescript = [
               "prettier"
+              "eslint_d"
             ];
             javascriptreact = [
               "prettier"
+              "eslint_d"
             ];
             typescriptreact = [
               "prettier"
+              "eslint_d"
             ];
 
             css = [ "prettier" ];
